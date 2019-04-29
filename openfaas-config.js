8776cc198b3358516d9e99a8bab0a4b0da77ea21
Franciszek Madej <f.madej@protonmail.com>
2019-04-29 12:04:21 +0200

    fix: add missing config options to fix function

--- openfaas-config.js
+++ openfaas-config.js
@@ -1,12 +1,14 @@
 module.exports = function(RED) {
   function FaasConfigNode(settings) {
     RED.nodes.createNode(this, settings);
+    this.protocol = settings.protocol;
     this.host = settings.host;
     this.port = settings.port;
-    this.functionsEndpoint = settings.functionsEndpoint;
+    this.endpoint = settings.endpoint;
     this.authType = settings.authType;
     this.jwtToken = settings.jwtToken;
     this.apiKey = settings.apiKey;
+    this.parse = settings.parse;
   }
 
   RED.nodes.registerType("openfaas-config", FaasConfigNode);
