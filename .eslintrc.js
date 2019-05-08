559997b2f7b88f68fdacf0e66f5ec94e92dbe913
Franciszek Madej <f.madej@protonmail.com>
2019-05-08 15:32:22 +0200

    chore: add Mocha envs to dismiss ESLint alerts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
+    mocha: true
   },
   plugins: ["mocha"],
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
@@ -12,8 +13,5 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 2018
-  },
-  rules: {
-    "mocha/no-exclusive-tests": "error"
   }
 };
