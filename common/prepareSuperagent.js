8a21d62bbbafafb783250b7638a5f6d65c6225b9
Franciszek Madej <f.madej@protonmail.com>
2019-05-16 09:30:03 +0200

    refactor: move superagent to separate file and use axios

---
+++ common/prepareSuperagent.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+module.exports = server => {
+  const requestUrl = `${server.protocol}://${server.host}:${server.port}/${
+    server.endpoint
+  }`;
+  const headers = {};
+  const auth = server.useAuth
+    ? { username: server.username, password: server.password }
+    : undefined;
+  if (server.auth === "jwt") {
+    headers["authorization"] = `Bearer ${server.jwt}`;
+  } else if (server.auth == "api") {
+    headers[server.header] = server.api;
+  }
+
+  const agent = auth
+    ? axios.create({
+        headers,
+        auth
+      })
+    : axios.create({ headers });
+
+  return { agent, requestUrl };
+};
